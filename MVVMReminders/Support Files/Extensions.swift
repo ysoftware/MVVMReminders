e4a7ee623efb9efe1b03cf5ae41a0b17b9ae96b5
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-07-12 11:37:14 +0300

    clean up

--- MVVMReminders/Support Files/Extensions.swift
+++ MVVMReminders/Support Files/Extensions.swift
@@ -8,28 +8,8 @@
 
 import Foundation
 
-public extension Encodable {
-
-	public var dictionary:[String:Any] {
-		do {
-			let data = try JSONEncoder().encode(self)
-			let value = try JSONDecoder().decode(JSONAny.self, from: data)
-			if let value_ = value.value as? [String:Any] {
-				return value_
-			}
-			else {
-				print("encodable.dictionary: something went wrong and needs debugging")
-				return [:]
-			}
-		}
-		catch let error {
-			print("encodable.dictionary: \(error)")
-			return [:]
-		}
-	}
-}
-
 extension Bool {
+	
 	mutating func toggle() {
 		self = !self
 	}
@@ -41,23 +21,5 @@ extension Array where Element:Equatable {
 	mutating func remove(object:Element) -> Element {
 		let i:Int = index(of: object)!
 		return remove(at: i)
-	}
-}
-
-extension Dictionary where Key == String {
-
-	func parse<T:Decodable>(object class:T.Type) -> T? {
-		return parse()
-	}
-
-	func parse<T:Decodable>() -> T? {
-		do {
-			let data = try JSONSerialization.data(withJSONObject: self)
-			return try JSONDecoder().decode(T.self, from: data)
-		}
-		catch {
-			print("search: parse \(T.self): \(error)")
-			return nil
-		}
 	}
 }
